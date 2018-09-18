eeac3da304b5353363c1ac28ef1019da8a9d91e0
jsphkm <jsphkm12@gmail.com>
2018-09-17 17:13:50 -0700

    Fix test

--- src/components/app.test.js
+++ src/components/app.test.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {shallow} from 'enzyme';
 import App from './app';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({adapter: new Adapter()})
 
 it('renders without crashing', () => {
-  // const div = document.createElement('div');
-  // ReactDOM.render(<App />, div);
-  // ReactDOM.unmountComponentAtNode(div);
   const dispatch = jest.fn();
-  shallow(<App title="Foo" lists={[]} dispatch={dispatch} />);
+  shallow(<App title="Foo" dispatch={dispatch} />);
 });
