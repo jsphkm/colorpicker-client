2bcf7f7176b2a3e3f583c4038b6e0f3d07875514
jsphkm <jsphkm12@gmail.com>
2018-09-17 17:29:00 -0700

    Fix test

--- src/components/app.test.js
+++ src/components/app.test.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Enzyme, shallow} from 'enzyme';
+import Enzyme from 'enzyme';
 import App from './app';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({adapter: new Adapter()})
 
 it('renders without crashing', () => {
-  const dispatch = jest.fn();
-  shallow(<App title="Foo" dispatch={dispatch} />);
+  Enzyme.shallow(<App title="Foo"/>);
 });
