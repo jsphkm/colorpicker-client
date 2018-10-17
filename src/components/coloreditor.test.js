0a307005718fa6b56ceb2d560ebbb3ad9a3b37db
jsphkm <jsphkm12@gmail.com>
2018-10-16 17:20:43 -0700

    Fix test

--- src/components/coloreditor.test.js
+++ src/components/coloreditor.test.js
@@ -3,7 +3,6 @@ import {shallow, mount} from 'enzyme';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import {Coloreditor} from './coloreditor';
-import {BrowserRouter as Router} from 'react-router-dom';
 
 Enzyme.configure({adapter: new Adapter()})
   describe('<Coloreditor />', () => {
@@ -41,8 +40,8 @@ Enzyme.configure({adapter: new Adapter()})
         lightness: 15
       }
     }
-    let checkedColorIndex = 0
-    let authToken = 'authToken'
+    let checkedColorIndex = 0;
+    let authToken = 'authToken';
     let store = {
       editor: 'editor',
       checkedColor: 'checkedColor',
@@ -63,24 +62,4 @@ Enzyme.configure({adapter: new Adapter()})
       match={{params: 1}}
     />);
   });
-
-  // it('Calls postPalette function onClick event of the save button', () => {
-  //   const onClick = jest.fn();
-  //   const dispatch = jest.fn().mockImplementation(() =>
-  //     Promise.resolve({colors: ['foo', 'bar']})
-  //   );
-  //   let wrapper = mount(<Router><Coloreditor
-  //     editor={editor}
-  //     checkedColor={checkedColor}
-  //     checkedColorIndex={checkedColorIndex}
-  //     authToken={authToken}
-  //     dispatch={dispatch}
-  //     match={{params: 1}}
-  //     onClick={onClick}
-  //     store={store}
-  //   /></Router>);
-      
-  //   wrapper.find('button.save-button').simulate('click');
-  //   expect(onClick).toHaveBeenCalled();
-  // });
 });
